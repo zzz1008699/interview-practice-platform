78597bd8112b550fe35b4bb041257add71bec854
Interview Developer <interview@example.com>
2026-03-28 13:57:37 +0800

    fix: router base path for deployment

--- src/router/index.ts
+++ src/router/index.ts
@@ -49,7 +49,7 @@ const routes: RouteRecordRaw[] = [
 
 // 创建路由实例
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
 
